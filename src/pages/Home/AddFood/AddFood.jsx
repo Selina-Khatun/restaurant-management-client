cca541a7399ce689b4a1e31bc87e170deca71c30
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-11-09 11:37:02 +0600

    add food item page

--- src/pages/Home/AddFood/AddFood.jsx
+++ src/pages/Home/AddFood/AddFood.jsx
@@ -1,9 +1,121 @@
 import React from 'react';
+import swal from 'sweetalert';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({ duration: 1000 });
 
 const AddFood = () => {
+    const handleAddFood = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const category = form.category.value;
+        const quantity = form.quantity.value;
+        const price = form.price.value;
+        const addByName = form.addByName.value;
+        const email = form.email.value;
+        const origin = form.origin.value;
+        const description = form.description.value;
+        const newProduct = { name, photo, category, quantity, price, addByName, email, origin, description };
+        console.log(newProduct);
+        fetch('http://localhost:5000/myItems', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newProduct)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    swal({
+                        title: "Good job!",
+                        text: "User added successfully!",
+                        icon: "success",
+                        button: "Aww yiss!",
+
+                    });
+                    form.reset();
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
     return (
         <div>
-            Add a food items
+            <section className="bg-white dark:bg-gray-900">
+                <div data-aos="zoom-in-up" className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                    <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new Item</h2>
+                    <form onSubmit={handleAddFood}>
+                        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                            <div className="sm:col-span-2">
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Item Name</label>
+                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Item name" required />
+                            </div>
+                            <div className="sm:col-span-2">
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product photo</label>
+                                <input type="url" name="photo" id="photo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder=" https://example.com" required />
+                            </div>
+                            <div>
+                                <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">category</label>
+                                <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" name='category' defaultValue='category '>
+                                    <option defaultValue="">Select category</option>
+                                    <option value="Makeup">soup</option>
+                                    <option value="Health">rice </option>
+                                    <option value="Beauty">tacos</option>
+                                    <option value="oil">Burger</option>
+                                    <option value="Cream">salad</option>
+                                    <option value="Cream">sea food</option>
+                                    <option value="Cream">sushi</option>
+                                    <option value="Cream">pasta</option>
+                                    <option value="Cream">curry</option>
+                                    <option value="Cream">pizza</option>
+                                    <option value="Cream">risotto</option>
+                                </select>
+                            </div>
+                            <div className="w-full">
+                                <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                                <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required />
+                            </div>
+                            <div>
+                                <label htmlFor="item-weight" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">quantity</label>
+                                <input type="number" name="quantity" id="quantity" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="12" required />
+                                <div className="w-full">
+                                    <label htmlFor="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
+                                    <input type="text" name="origin" id="origin" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="origin" required />
+                                </div>
+
+                            </div>
+
+                            <div>
+                                <div className="w-full">
+                                    <label htmlFor="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add by email</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your name" required />
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add by Name</label>
+                                    <input type="text" name="addByName" id="addByName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your name" required />
+                                </div>
+                            </div>
+
+
+
+                            <div className="sm:col-span-2">
+                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                                <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" name='description' placeholder="Your description here"></textarea>
+                            </div>
+
+                        </div>
+
+                        <button type="submit" className="w-full my-4 text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">Add Item</button>
+
+
+                    </form>
+                </div>
+            </section>
         </div>
     );
 };
